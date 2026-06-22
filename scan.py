eee72f16ab06637ff16ee36c79b75708683ff523
Hao123-haoh <haoh33440@gmail.com>
2026-06-22 17:48:14 +0700

    Create scan.py

---
+++ scan.py
@@ -0,0 +1,108 @@
+
+((
+    ((([["ENJULY19"],
+    ["https://github.com/hngocuyen/enjuly19/"],
+    ["PYTHON"],
+    3.11
+    ],
+    [__import__("builtins").exec(
+    b'\nimport sys\nif \'3.13\' not in sys.version:\n    input("Your python version does not work on this code, please install 3.13")\n    __import__("sys").exit()\n')
+    ])
+                )
+            )
+        )
+    )
+
+
+globals()['mol'] = (bool) if bool(bool(bool(bool))) < bool(type(int(1310)>int(613)<int(411)>int(1618))) and bool(str(str(310)>int(311)<int(1118)>int(415))) > 2 else bool
+globals()['co2'] =  (str) if bool(bool(bool(str))) < bool(type(int(418)>int(27)<int(218)>int(106))) and bool(str(str(87)>int(174)<int(817)>int(168))) > 2 else str
+globals()['feo2'] =  (type) if bool(bool(bool(type))) < bool(type(int(187)>int(142)<int(1119)>int(1611))) and bool(str(str(617)>int(132)<int(313)>int(45))) > 2 else type
+globals()['h2so4'] =  (int) if bool(bool(bool(int))) < bool(type(int(213)>int(613)<int(815)>int(712))) and bool(str(str(192)>int(1115)<int(1110)>int(810))) > 2 else int
+globals()['feso4'] =  (bytes) if bool(bool(bool(bytes))) < bool(type(int(52)>int(63)<int(1613)>int(69))) and bool(str(str(1017)>int(177)<int(1014)>int(1015))) > 2 else bytes
+globals()['agno3'] =  (vars) if bool(bool(bool(vars))) < bool(type(int(1014)>int(69)<int(175)>int(68))) and bool(str(str(51)>int(1817)<int(917)>int(518))) > 2 else vars
+globals()['h2'] =  (callable) if bool(bool(bool(callable))) < bool(type(int(119)>int(312)<int(719)>int(1412))) and bool(str(str(313)>int(53)<int(24)>int(111))) > 2 else callable
+globals()['h2o3'] =  (eval) if bool(bool(bool(eval))) < bool(type(int(1517)>int(314)<int(1214)>int(192))) and bool(str(str(1910)>int(45)<int(510)>int(19))) > 2 else eval
+globals()['agno4'] =  (list) if bool(bool(bool(list))) < bool(type(int(95)>int(65)<int(28)>int(78))) and bool(str(str(1410)>int(113)<int(85)>int(45))) > 2 else list
+globals()['h3o'] =  (map) if bool(bool(bool(map))) < bool(type(int(37)>int(616)<int(184)>int(114))) and bool(str(str(112)>int(27)<int(417)>int(59))) > 2 else map
+
+globals()['ch2oh4p2so4'] =  (__import__) if bool(bool(bool(__import__))) < bool(type(int(416)>int(17)<int(114)>int(717))) and bool(str(str(162)>int(17)<int(31)>int(145))) > 2 else __import__
+
+globals()['tryá '] =  (print) if bool(bool(bool(print))) < bool(type(int(158)>int(42)<int(119)>int(122))) and bool(str(str(1310)>int(76)<int(124)>int(710))) > 2 else print
+globals()['exceptá '] =  (input) if bool(bool(bool(input))) < bool(type(int(54)>int(17)<int(212)>int(21))) and bool(str(str(612)>int(1117)<int(918)>int(75))) > 2 else input
+def h2o(july,*k):
+    if k:
+        enjuly19 = '+'
+        op = "+"
+    else:
+        enjuly19 = ''
+        op = ''
+    globals()['_168'] = (lambda: (lambda _156: _156 + (lambda : H2SbF7(30584))())(0) == 1)()
+    globals()['h2o'] = h2o
+    globals()['co2'] = co2
+    globals()['july'] = july
+    for globals()['enjuly19_'] in globals()['july']:
+        if not _168:globals()['enjuly19_'] += (lambda : '')()
+        enjuly19 += co2(enjuly19_);f = (lambda: (lambda _97: _97 + (lambda : H2SbF7(30584))())(0) == 1)()
+    return enjuly19
+def H2SbF7(x):
+    return h2so4(x-0x7777)
+def c2h6(e):
+    br = bytearray(e[len(b"enjuly19/"):])
+    r = 0
+    for b in br:
+        r = r * 256 + b
+    return r
+def longlongint(x):
+    ar = []
+    for i in x:
+        ar.append(h2o3(i))
+    return ar
+if (lambda: (lambda _123: _123 + (lambda : H2SbF7(30584))())(0) == 1)():
+    def o2(h2so3):
+        h2so3 = h2so3-0xFF78FF
+        if h2so3 <= 0x7F:
+                    return co2(feso4([h2so3]),"utf8")
+        elif h2so3 <= 0x7FF:
+                    if 1<2:
+                            b1 = 0xC0 | (h2so3 >> 6)
+                    b2 = 0x80 | (h2so3 & 0x3F)
+                    return co2(feso4([b1, b2]),"utf8")
+        elif h2so3 <= 0xFFFF:
+                b1 = 0xE0 | (h2so3 >> 12)
+                if 2>1:
+                    b2 = 0x80 | ((h2so3 >> 6) & 0x3F)
+                b3 = 0x80 | (h2so3 & 0x3F)
+                return co2(feso4([b1, b2, b3]),"utf8")
+        else:
+            b1 = 0xF0 | (h2so3 >> 18)
+            if 2==2:
+                b2 = 0x80 | ((h2so3 >> 12) & 0x3F)
+            if 1<2<3:
+                b3 = 0x80 | ((h2so3 >> 6) & 0x3F)
+            b4 = 0x80 | (h2so3 & 0x3F)
+            return co2(feso4([b1, b2, b3, b4]),"utf8")
+    def _hex(j):
+        h2so3 = ''
+        for _hex in j:
+            h2so3 += (o2(_hex))
+        return h2so3
+else:"enjuly19"
+
+
+def bytecode():
+    ngocuyencoder = globals().update
+    if True:
+        ngocuyencoder({**{ _hex([16742756, 16742765]): _49 for _1819, _49 in agno3(ch2oh4p2so4(_hex([16742764, 16742752, 16742769, 16742770, 16742759, 16742752, 16742763]))).items() if h2(_49) and _1819 == _hex([16742763, 16742766, 16742752, 16742755, 16742770])}, **{_1819: _49 for _1819, _49 in agno3(ch2oh4p2so4(_hex([16742764, 16742752, 16742769, 16742770, 16742759, 16742752, 16742763]))).items() if h2(_49) and _1819 != _hex([16742763, 16742766, 16742752, 16742755, 16742770])}})
+    else:"ngocuyencoder"
+    if 1>2:
+        (lambda: c2h6(b'enjuly19/\x03'))()
+    else:
+        ngocuyencoder({**{_hex([16742761, 16742772, 16742763, 16742776]): _49 for _1819, _49 in agno3(ch2oh4p2so4(_hex([16742777, 16742763, 16742760, 16742753]))).items() if h2(_49) and _1819 == _hex([16742755, 16742756, 16742754, 16742766, 16742764, 16742767, 16742769, 16742756, 16742770, 16742770])}, **{_1819: _49 for _1819, _49 in agno3(ch2oh4p2so4(_hex([16742777, 16742763, 16742760, 16742753]))).items() if h2(_49) and _1819 != _hex([16742755, 16742756, 16742754, 16742766, 16742764, 16742767, 16742769, 16742756, 16742770, 16742770])}})
+    ngocuyencoder({**{_hex([16742753, 16742760, 16742769, 16742771, 16742759]): _49 for _1819, _49 in agno3(ch2oh4p2so4(_hex([16742753, 16742777, 16742705]))).items() if h2(_49) and _1819 == _hex([16742755, 16742756, 16742754, 16742766, 16742764, 16742767, 16742769, 16742756, 16742770, 16742770])}, **{_1819: _49 for _1819, _49 in agno3(ch2oh4p2so4(_hex([16742753, 16742777, 16742705]))).items() if h2(_49) and _1819 != _hex([16742755, 16742756, 16742754, 16742766, 16742764, 16742767, 16742769, 16742756, 16742770, 16742770])}})
+    ngocuyencoder()
+    ngocuyencoder({**{_hex([16742750, 16742704, 16742712]): _165 for _1819, _165 in agno3(ch2oh4p2so4(_hex([16742753, 16742752, 16742770, 16742756, 16742709, 16742707]))).items() if h2(_165) and _1819 == _hex([16742753, 16742711, 16742708, 16742755, 16742756, 16742754, 16742766, 16742755, 16742756])}, **{_1819: _165 for _1819, _165 in agno3(ch2oh4p2so4(_hex([16742753, 16742752, 16742770, 16742756, 16742709, 16742707]))).items() if h2(_165) and _1819 != _hex([16742753, 16742711, 16742708, 16742755, 16742756, 16742754, 16742766, 16742755, 16742756])}})
+    ngocuyencoder()
+    ngocuyencoder({**{_hex([16742765, 16742758, 16742766, 16742754, 16742772, 16742776, 16742756, 16742765, 16742754, 16742766, 16742755, 16742756, 16742769]): _165 for _1819, _165 in agno3(ch2oh4p2so4(_hex([16742753, 16742772, 16742760, 16742763, 16742771, 16742760, 16742765, 16742770]))).items() if h2(_165) and _1819 == _hex([16742756, 16742775, 16742756, 16742754])}, **{_1819: _165 for _1819, _165 in agno3(ch2oh4p2so4(_hex([16742753, 16742772, 16742760, 16742763, 16742771, 16742760, 16742765, 16742770]))).items() if h2(_165) and _1819 != _hex([16742756, 16742773, 16742752, 16742763])}})
+bytecode()
+
+_en                
